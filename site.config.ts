97e1eef7716d657567be1730949d5e3fd36e3f56
Jialin <44021604+Zjlkk@users.noreply.github.com>
2022-07-25 16:33:16 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -14,7 +14,7 @@ export default siteConfig({
   author: 'Jialin',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'My thoughts on Economics, Products and Start-ups',
 
   // social usernames (optional)
   twitter: 'jjialinn',
